c2b2a5fd4e23044c5a8fa93494df47677bf5bddb
M1urray <rknjonjo@gmail.com>
2023-11-03 10:30:58 +0300

    End of section 8

--- API/Extentions/ApplicationServiceExtention.cs
+++ API/Extentions/ApplicationServiceExtention.cs
@@ -18,6 +18,8 @@ namespace API.Extentions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             return services;
         }
     }
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -24,7 +24,7 @@ namespace API.Middleware
             {
                 await _next(context);
             }
-
+ 
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
